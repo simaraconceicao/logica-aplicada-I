64639324be4dcc74135ec922dc4847d0203f6280
simaraconceicao <contato@simaraconceicao.com>
2021-05-21 20:36:05 -0300

    inclui respostas

--- app2.js
+++ app2.js
@@ -64,6 +64,59 @@ function fatorial(n) {
 console.log(fatorial(3))
 
 
+//Crie uma função que recebe 2 parâmetros e retorna a multiplicação deles.
+
+function multiplica(a,b) {
+    return a * b
+}
+
+console.log(multiplica(2,5))
+
+// Crie uma função que calcula 5% de desconto retornando o valor do desconto.
+
+function desconta(valor) {
+    let desconto = valor * (5/100)
+    return desconto
+}
+
+console.log(desconta(100))
+
+//Crie uma função que irá converter uma quantia de real para dolar utilizando a cotação do dia.
+
+function converteRealDolar(valor) {
+    let dolar = 5.37
+    let resultado = valor * dolar
+    return resultado
+}
+
+console.log(converteRealDolar(100))
+
+//Utilize a estrutura de repetição for para imprimir no console  conforme instruções: 
+
+// a) números de 1 a 100  
+for (i = 1; i <= 100 ; i++) {
+    console.log(i)
+}
+// b) quando chegar no número 50 interrompa a instrução e pare o loop 
+for (i = 1; i <= 100 ; i++) {
+    if ( i === 51 ) {
+        break
+    }
+    console.log(i)
+}
+
+// c) quando chegar no número 50 pule a instrução|
+
+for (i = 1; i <= 100 ; i++) {
+    if ( i === 50 ) {
+        continue
+    }
+    console.log(i)
+}
+
+
+
+// imprima na tela a partir de 20 e depois de 10 em 10, em vez dos numeros 60 e 90 imprima a palavra continue.
 
 let x = 10;
 
@@ -76,6 +129,22 @@ while(x < 100) {
     continue;
   }
 
-  console.log("Testando continue " + x);
+  console.log(x);
 
 }
+
+
+// Para revisao de quarta:
+
+// | Exemplo 12 | Descrição |
+// | --- | --- |
+// | `Piramide` | Construa uma piramide com hashtags, a piramide é espelhada e tem um espaço no meio e recebe um número que determina sua altura.
+// ```
+// Height: 4
+//    #  #
+//   ##  ##
+//  ###  ###
+// ####  ####
+// ```
+// <img src ="https://www.researchgate.net/profile/Michael-Green-45/publication/334209333/figure/fig1/AS:782831043244032@1563652873593/Super-Mario-Bros-scene-where-Mario-need-to-jump-over-a-gap-from-the-first-pyramid-to-the_Q320.jpg"/>
+// |
